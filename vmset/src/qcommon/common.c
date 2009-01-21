300dc2c36e449c0ec69f86ae01ab042061066b78
I.Am.Gewgle <I.Am.Gewgle@243143e6-e6bf-11dd-bd7b-b12b6590754f>
2009-01-21 01:44:28 +0000

    Backported client and vmset

--- vmset/src/qcommon/common.c
+++ vmset/src/qcommon/common.c
@@ -33,7 +33,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #endif
 
 int demo_protocols[] =
-{ PROTOCOL_VERSION, 0 };
+{ 66, 67, 68, 69, 0 };
 
 #define MAX_NUM_ARGVS	50
 
